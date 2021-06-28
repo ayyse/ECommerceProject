4b46e8027ac73e8dbdb534bdb1149e4cb13ea047
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2021-06-28 15:49:16 +0300

    boilerplate template

---
+++ aspnet-core/src/eCommerceProject.EntityFrameworkCore/EntityFrameworkCore/eCommerceProjectDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using eCommerceProject.Authorization.Roles;
+using eCommerceProject.Authorization.Users;
+using eCommerceProject.MultiTenancy;
+
+namespace eCommerceProject.EntityFrameworkCore
+{
+    public class eCommerceProjectDbContext : AbpZeroDbContext<Tenant, Role, User, eCommerceProjectDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public eCommerceProjectDbContext(DbContextOptions<eCommerceProjectDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
