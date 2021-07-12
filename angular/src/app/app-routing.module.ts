3559cf047796870025ac3fe91760f54a7c9e854c
Ayşe Akışık <62264505+ayyse@users.noreply.github.com>
2021-07-12 22:45:50 +0300

    routing
    website starting customer home page

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
+import { CustomerHomeComponent } from './customer-components/customer-home/customer-home.component';
+import { CommonModule } from '@angular/common';
 import { DetailUserProductComponent } from './userProducts/detail-user-product/detail-user-product.component';
 import { UserProductComponent } from './userProducts/user-product/user-product.component';
 import { TypeComponent } from './productTypes/type/type.component';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AppRouteGuard } from '@shared/auth/auth-route-guard';
 import { HomeComponent } from './home/home.component';
@@ -15,20 +17,26 @@ import { ProductComponent } from './products/product/product.component';
 import { BrandComponent } from './productBrands/brand/brand.component';
 import { ColorComponent } from './productColors/color/color.component';
 
+const routes: Routes = [
+    { path: 'customerhome', component: CustomerHomeComponent },
+    { path: 'detail/:productId', component: DetailUserProductComponent },
+    { path: 'shop', component: UserProductComponent },
+]
+
 @NgModule({
     imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
         RouterModule.forChild([
             {
                 path: '',
                 component: AppComponent,
                 children: [
                     { path: 'home', component: HomeComponent, canActivate: [AppRouteGuard] },
-                    { path: 'detail/:productId', component: DetailUserProductComponent, canActivate: [AppRouteGuard] },
                     { path: 'products', component: ProductComponent, canActivate: [AppRouteGuard] },
                     { path: 'brands', component: BrandComponent, canActivate: [AppRouteGuard] },
                     { path: 'types', component: TypeComponent, canActivate: [AppRouteGuard] },
                     { path: 'colors', component: ColorComponent, canActivate: [AppRouteGuard] },
-                    { path: 'shop', component: UserProductComponent, canActivate: [AppRouteGuard] },
                     { path: 'users', component: UsersComponent, data: { permission: 'Pages.Users' }, canActivate: [AppRouteGuard] },
                     { path: 'roles', component: RolesComponent, data: { permission: 'Pages.Roles' }, canActivate: [AppRouteGuard] },
                     { path: 'tenants', component: TenantsComponent, data: { permission: 'Pages.Tenants' }, canActivate: [AppRouteGuard] },
